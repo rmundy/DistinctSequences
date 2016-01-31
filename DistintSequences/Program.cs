37d123dff9c18c4920819d1aa5f832a7d8d6e99d
Bobby Mundy <robert.l.mundy@gmail.com>
2016-01-31 12:01:08 -0500

    Initial Check in

---
+++ DistintSequences/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DistintSequences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var fs = File.Open(args[0], FileMode.Open, FileAccess.Read))
+            {
+                using (var bs = new BufferedStream(fs))
+                {
+                    using (var sr = new StreamReader(bs))
+                    {
+                        String line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            var split = line.Split(',');
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
